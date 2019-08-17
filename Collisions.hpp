f51b767d6979e5884637be538166c744c311b529
blakeanedved <blakeanedved@gmail.com>
2019-08-16 23:16:50 -0500

    Refactor and restructure of codebase

--- Collisions.hpp
+++ Collisions.hpp
@@ -4,16 +4,19 @@
 
 namespace Shade {
 	namespace Collisions {
-		auto AABB(Shade::Entity& e1, Shade::Entity& e2) -> bool {
-			std::vector<float> a1 = e1.GetAABB();
-			std::vector<float> a2 = e2.GetAABB();
-
-			// a = {min_x, min_y, min_z, max_x, max_y, max_z}
-			if (a1[3] < a2[0] || a1[0] > a2[3]) return false;
-			if (a1[4] < a2[1] || a1[1] > a2[4]) return false;
-			if (a1[5] < a2[2] || a1[2] > a2[5]) return false;
-			return true;
-		}
+		auto AABB(Shade::Entity& e1, Shade::Entity& e2) -> bool;
 	}
 }
+
+auto AABB(Shade::Entity& e1, Shade::Entity& e2) -> bool {
+	std::vector<float> a1 = e1.GetAABB();
+	std::vector<float> a2 = e2.GetAABB();
+
+	// a = {min_x, min_y, min_z, max_x, max_y, max_z}
+	if (a1[3] < a2[0] || a1[0] > a2[3]) return false;
+	if (a1[4] < a2[1] || a1[1] > a2[4]) return false;
+	if (a1[5] < a2[2] || a1[2] > a2[5]) return false;
+	return true;
+}
+
 #endif
--- Entity.hpp
+++ Entity.hpp
@@ -6,9 +6,6 @@
 #include <string>
 
 namespace Shade {
-	class Entity;
-	std::map<std::string, Shade::Entity*> entity_table;
-	std::map<std::string, std::vector<Shade::Entity*>> type_table;
 	class Entity {
 		public:
 			std::string name;
@@ -19,52 +16,65 @@ namespace Shade {
 			std::vector<float> AABB;
 
 		public:
-			Entity(std::string name, std::string type, std::vector<float> vertices){
-				if (entity_table.find(name) != entity_table.end()){
-					std::cout << "Entity " << name << " already exists" << std::endl;
-					exit(1);
-				}
-				entity_table.insert(std::make_pair(name, this));
-				if (auto tt = type_table.find(type); tt == type_table.end()){
-					type_table.insert(std::make_pair(type, std::vector<Shade::Entity*>{this}));
-				} else {
-					tt->second.push_back(this);
-				}
-				this->name = std::move(name);
-				this->type = std::move(type);
-				this->vertices = vertices;
-				this->AABB = std::vector<float>(6);
-				this->GenerateAABB();
-			}
-			~Entity(){
-				if (entity_table.find(this->name)->second == this){
-					entity_table.erase(this->name);
-					auto temp = type_table.find(this->type);
-					temp->second.erase(std::find(temp->second.begin(), temp->second.end(), this));
-				}	
-			}
-			auto GetVertices() -> std::vector<float> {
-				return this->vertices;
-			}
-			auto GetAABB() -> std::vector<float> {
-				return this->AABB;
-			}
-			auto GenerateAABB() -> void {
-				this->AABB[0] = this->vertices[0];
-				this->AABB[3] = this->vertices[0];
-				this->AABB[1] = this->vertices[1];
-				this->AABB[4] = this->vertices[1];
-				this->AABB[2] = this->vertices[2];
-				this->AABB[5] = this->vertices[2];
-				for (int i = 3; i < this->vertices.size(); i+=3){
-					this->AABB[0] = std::min(this->vertices[i], this->AABB[0]);
-					this->AABB[1] = std::min(this->vertices[i+1], this->AABB[1]);
-					this->AABB[2] = std::min(this->vertices[i+2], this->AABB[2]);
-					this->AABB[3] = std::max(this->vertices[i], this->AABB[3]);
-					this->AABB[4] = std::max(this->vertices[i+1], this->AABB[4]);
-					this->AABB[5] = std::max(this->vertices[i+2], this->AABB[5]);
-				}
-			}
+			Entity(std::string name, std::string type, std::vector<float> vertices);
+			~Entity();
+			auto GetVertices() -> std::vector<float>;
+			auto GetAABB() -> std::vector<float>;
+			auto GenerateAABB() -> void;
 	};
+	std::map<std::string, Shade::Entity*> entity_table;
+	std::map<std::string, std::vector<Shade::Entity*>> type_table;
 }
+
+Shade::Entity::Entity(std::string name, std::string type, std::vector<float> vertices){
+	if (entity_table.find(name) != entity_table.end()){
+		std::cout << "Entity " << name << " already exists" << std::endl;
+		exit(1);
+	}
+	entity_table.insert(std::make_pair(name, this));
+	if (auto tt = type_table.find(type); tt == type_table.end()){
+		type_table.insert(std::make_pair(type, std::vector<Shade::Entity*>{this}));
+	} else {
+		tt->second.push_back(this);
+	}
+	this->name = std::move(name);
+	this->type = std::move(type);
+	this->vertices = vertices;
+	this->AABB = std::vector<float>(6);
+	this->GenerateAABB();
+}
+
+Shade::Entity::~Entity(){
+	if (entity_table.find(this->name)->second == this){
+		entity_table.erase(this->name);
+		auto temp = type_table.find(this->type);
+		temp->second.erase(std::find(temp->second.begin(), temp->second.end(), this));
+	}
+}
+
+auto Shade::Entity::GetVertices() -> std::vector<float> {
+	return this->vertices;
+}
+
+auto Shade::Entity::GetAABB() -> std::vector<float> {
+	return this->AABB;
+}
+
+auto Shade::Entity::GenerateAABB() -> void {
+	this->AABB[0] = INT_MAX;
+	this->AABB[1] = INT_MAX;
+	this->AABB[2] = INT_MAX;
+	this->AABB[3] = INT_MIN;
+	this->AABB[4] = INT_MIN;
+	this->AABB[5] = INT_MIN;
+	for (int i = 0; i < this->vertices.size(); i+=3){
+		this->AABB[0] = std::min(this->vertices[i], this->AABB[0]);
+		this->AABB[1] = std::min(this->vertices[i+1], this->AABB[1]);
+		this->AABB[2] = std::min(this->vertices[i+2], this->AABB[2]);
+		this->AABB[3] = std::max(this->vertices[i], this->AABB[3]);
+		this->AABB[4] = std::max(this->vertices[i+1], this->AABB[4]);
+		this->AABB[5] = std::max(this->vertices[i+2], this->AABB[5]);
+	}
+}
+
 #endif
